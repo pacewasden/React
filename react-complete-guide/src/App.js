0a0452ab8f6651f4896bb4037990743829190ebe
Pace Wasden <pacewasden@gmail.com>
2020-11-12 18:27:48 -0700

    Is css the worst? Yes it is

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -15,16 +15,47 @@ class App extends Component {
   switchNameHandler =()=>{
     console.log("Button was clicked");
   }
+  nameChangeHandler =(event)=>{
+    this.setState({
+      person: [
+        {name:"Pace", age:"29"},
+        //Dynamically changing information 
+        {name:event.target.value, age:"39"},
+        {name:"Jack", age:"9"}
+      ]
+    })
+  }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8px'
+    };
+
+    style.backgroundColor= 'red';
+
+
     return (
       <div className="App">
-        <h1>Hi I am a react app</h1>
+        <h1>Meet the Barbers Page</h1>
         <p>This is still working</p>
-        <button onClick={this.switchNameHandler}> Switch Person </button>
-        <Person name={this.state.person[0].name} age= {this.state.person[0].age}/ >
-        <Person click= {this.switchNameHandler} name={this.state.person[1].name} age= {this.state.person[1].age}> My hobbies include golf </Person>
-        <Person name={this.state.person[2].name} age= {this.state.person[2].age}/>
+        <button style= {style} onClick={this.switchNameHandler}> Switch Person </button>
+        <Person 
+          name={this.state.person[0].name}
+          age= {this.state.person[0].age}
+         / >
+        <Person 
+          click= {this.switchNameHandler} 
+          name={this.state.person[1].name} 
+          age= {this.state.person[1].age}
+          changed={this.nameChangeHandler}> My hobbies include golf 
+        </Person>
+        <Person 
+          name={this.state.person[2].name} 
+          age= {this.state.person[2].age}
+        />
       </div>
     );
   }
